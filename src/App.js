b703000821c1ac147b4a8357d3fa3786feb00537
Debkanta Mondal <46219855+DebkantaMondal@users.noreply.github.com>
2021-09-13 13:40:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ function App() {
                 <div className="col-md-9">
                   <ul>
                     <li>
-                      <Link to="/">Home</Link>
+                      <Link to="/AssignmentFoodSite">Home</Link>
                     </li>
                     <li>
-                      <Link to="/signup">Sign Up</Link>
+                      <Link to="/AssignmentFoodSite/signup">Sign Up</Link>
                     </li>
                   </ul>
                 </div>
@@ -36,10 +36,10 @@ function App() {
 
 
           <Switch>
-            <Route exact path="/">
+            <Route exact path="/AssignmentFoodSite">
               <Home />
             </Route>
-            <Route path="/signup">
+            <Route path="/AssignmentFoodSite/signup">
               <Signup />
             </Route>
           </Switch>
